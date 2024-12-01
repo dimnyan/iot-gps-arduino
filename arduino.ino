d80dfb8bc8b82a00ef914402200ce2476b4765ed
Nyoman Lanang <dimasnyoman1@gmail.com>
2024-12-01 20:27:57 +0700

    new code

--- arduino.ino
+++ arduino.ino
@@ -1,19 +1,30 @@
-#include <SoftwareSerial.h>
-SoftwareSerial GPRS(7, 8) // RX, TX
 
-void setup() {
-    Serial.begin(9600);
-    GPRS.begin(9600);
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
 }
 
-void loop() {
-    while(GPRS.available())
-    {
-        Serial.write(GPRS.read());
+void loop(){
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
     }
-
-    while(Serial.available())
-    {
-        GPRS.write(Serial.read());
-    }
+  }
 }
